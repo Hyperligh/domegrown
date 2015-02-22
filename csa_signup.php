a01a2ec3dfd97b8d5f02e874eacb152baf31f126
Hyperligh <hyperligh19@hotmail.com>
2015-02-22 12:13:54 -0800

    Moved to local server

---
+++ csa_signup.php
@@ -0,0 +1,116 @@
+<!DOCTYPE html>
+<html lan="en-US">
+<head>
+	<meta name="description" content="Redmond, OR local organic farm that has CSA for pickup near Bend | Dome Grown Produce">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<title>Local CSA organic farm in Redmond, OR | Dome Grown Produce</title>
+	<link rel="stylesheet" href="style/domegrownstyle.css">
+	<link href='http://fonts.googleapis.com/css?family=Roboto' rel='stylesheet'>
+	<script src="js/jquery-1.11.2.min.js"></script>
+	
+
+</head>
+
+<body>
+	<div id="container">
+		<header>
+			<div id="top-head">	
+				<a href="/"><img class="logo" src="photos/dpg-bend-or-farms-logo-transparent.png" alt="Dome Grown Produce has year round vegetables in Bend, OR"></a>
+				<div id="tagline">
+				<h1>"Come on down and get your<br><span> homegrown at Dome Grown"</span></h1></div>
+					<h4>Phone Number: 541-678-3064</h4>
+				
+			</div>
+					<nav id="main-nav">
+						<ul>
+							<li><a href="#">About Us</a></li>
+							<li><a href="#">CSA</a></li>
+							<li><a href="#">Photo Gallery</a></li>
+							<li><a href="#">Recipes</a></li>
+							<li><a href="#">Events</a></li>
+
+						</ul>
+					</nav>
+				
+				
+					<div class="interior-hero" id="interior-hero">
+						
+					</div>
+				
+				
+								<div id="cta">
+									<ul>
+										<li><a href="#">Contact Us</a></li>
+										<li><a class="anchor" href="pdf/dome-grown-produce-csa-signup-brochure.pdf" target="_blank">CSA Signup</a></li>
+									</ul>
+								</div>
+
+		</header>
+				
+				
+					<div class="row1-content cf">
+						<div class="left-65 left-row-1">
+								<h1><span>Place holder</span></h1>
+								
+
+							<p>Place holder
+							</p>
+							
+
+					
+						</div>
+								<div class="right-35 right-row-1 center ">
+									<h3>Dome Grown Produce</h3>
+									<p>7858 SW 61st St.</p>
+									<p>Redmond, OR 97756</p>
+									<p>541-678-3064</p>
+									<a href="https://www.facebook.com/domegrown" target="_blank"><img class ="social-icons" src="photos/svg-social-icons/facebook29.svg"></a>
+									<!--<a href="https://plus.google.com/u/0/111212075289834648742/posts" target="_blank"><img class="social-icons" src="photos/svg-social-icons/google29.svg"></a>-->
+									
+								</div>
+
+					</div>
+				
+
+							<div class="row2-content cf">
+
+								<div class="left-65">
+										<h2>Place holder</h2>
+
+									<p>
+								  
+									</p>
+								</div>
+										<div class="right-35 right-row-2 ">
+											<h3 class="center">Community Supported Agriculture</h3>
+												<p>
+													Sign up here for CSA packages by downloading the brochure. Just print and mail today.
+													<p><a class="anchor" href="pdf/dome-grown-produce-csa-signup-brochure.pdf" target="_blank">Download Brochure Here.</a></p>
+
+												</p>
+										</div>
+
+							</div>
+							
+
+
+			
+					<footer class="cf">
+									<div class="bottom-border"></div>
+						<h3>Dome Grown Produce</h3>
+						<p>7858 SW 61st St.</p>
+						<p>Redmond, OR 97756</p>
+						<p>541-678-3064</p>
+							<a href="https://www.facebook.com/domegrown" target="_blank"><img class ="footer-social-icons" src="photos/svg-social-icons/facebook29.svg"></a>
+							<!--<a href="https://plus.google.com/u/0/111212075289834648742/posts" target="_blank"><img class="footer-social-icons" src="photos/svg-social-icons/google29.svg"></a>-->
+								
+
+						
+
+					</footer>
+					
+
+	</div>
+</body>
+</html>
